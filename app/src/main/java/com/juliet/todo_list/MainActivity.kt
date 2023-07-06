ad368a811d95b572e780724373bb6a95cebf458f
Juliet-Olieng <anyangojolieng@gmail.com>
2023-07-06 10:21:38 +0300

    todolist

--- app/src/main/java/com/juliet/todo_list/MainActivity.kt
+++ app/src/main/java/com/juliet/todo_list/MainActivity.kt
@@ -10,34 +10,50 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var todoAdapter: TodoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.btnAdd.setOnClickListener{
-            val todoTitle =binding.etTodo.text.toString()
-            if (todoTitle.isNotEmpty()){
-                val todo =Todo(todoTitle)
+
+
+        binding.btnAdd.setOnClickListener {
+            val todoTitle = binding.etTodo.text.toString()
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
                 todoAdapter.addTodo(todo)
                 binding.etTodo.text.clear()
             }
         }
-        binding.btnDelete.setOnClickListener{
+        binding.btnDelete.setOnClickListener {
 //            val deleteList=binding.btnDelete.text.toString()
             todoAdapter.deleteDoneTodos()
         }
     }
+
     override fun onResume() {
         super.onResume()
+        initList()
         displayList()
+
+
     }
-    fun displayList(){
-        var todo1=Todo("learning kotlin",false)
-        var todo2=Todo("revising past questions",true)
-        var todo3 =Todo("watching a movie",false)
-        var todo4=Todo("creating an app",true)
-        var todos= mutableListOf<Todo>(todo1,todo2,todo3,todo4)
-       binding.rvTodo.layoutManager=LinearLayoutManager(this)
-       var todoAdapter=TodoAdapter(todos)
-        binding.rvTodo.adapter=todoAdapter
+
+    private fun makeToDoList(): List<Todo> {
+        val todo1 = Todo("learning kotlin", false)
+        val todo2 = Todo("revising past questions", true)
+        val todo3 = Todo("watching a movie", false)
+        val todo4 = Todo("creating an app", true)
+        return mutableListOf(todo1, todo2, todo3, todo4)
+    }
+
+    private fun initList() {
+        val todoList = makeToDoList()
+        todoAdapter = TodoAdapter(todoList.toMutableList())
+    }
+
+    private fun displayList() {
+        binding.rvTodo.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = todoAdapter
+        }
     }
 }
--- app/src/main/java/com/juliet/todo_list/Todo.kt
+++ app/src/main/java/com/juliet/todo_list/Todo.kt
@@ -2,7 +2,7 @@ package com.juliet.todo_list
 
 data class Todo (
     val title:String,
-    val isChecked:Boolean=false
+    val isChecked:Boolean=true
         )
 
 
